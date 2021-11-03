dda9833a4794f844731e7b0115ae4786fc51b099
zsigrib <zsigrib@gmail.com>
2021-11-03 18:22:48 +0100

    Add markdown to docstrings in consoleapp.py

--- consoleapp.py
+++ consoleapp.py
@@ -7,57 +7,60 @@ import re
 from enum import Enum
 class ConsoleApp:
     '''Each instance of this class is a textual input parser, which makes it easy to interpret variable queries, variable setting commands,
-    and function calls. When iterated over (or when .input() is called), it requests input from stdin (with a custom prompt), and returns 
-    the parsed information, an action, rname, data tuple. 
-    'action' tells what type of action did the user make: 'get_var' for a variable query, 'set_var' for a variable setting command, 'func' for
-        a function call, 'fail_func' for an improperly formatted function call, 'fail_var' for an improperly formatted command regarding a
-        variable, and 'nonsense' if the input could not be interpreted
-    'rname' is the name of the variable/command that 'action' refers to
-    'data' is additional data: None if not applicable, the input string if 'fail_...' occured, the extracted string representation of the
-        variable on 'set_var', and the inputs of the function in case of 'func' in a dictionary
-    For more information on these, see ConsoleApp.parse()'s docstring.
+    and function calls. When iterated over (or when `.input()` is called), it requests input from `stdin` (with a custom prompt), and returns 
+    the parsed information, an `action`, `rname`, `data` tuple.\n
+    -   `action` tells what type of action did the user make: 
+        `'get_var'` for a variable query, `'set_var'` for a variable setting command, `'func'` for a function call, `'fail_func'` for an 
+        improperly formatted function call, `'fail_var'` for an improperly formatted command regarding a variable, and `'nonsense'` if the 
+        input could not be interpreted\n
+    -   `'rname'` is the name of the variable/command that 'action' refers to\n
+    -   `'data'` is additional data: `None` if not applicable, the input string if `'fail_...'` occured, the extracted string representation 7
+        of the variable on `'set_var'`, and the inputs of the function in a dictionary in case of `'func'`\n
+    For more information on these, see `ConsoleApp.parse()`'s docstring.\\
     The class uses regexes heavily. The 'r'names of variables/functions are actually regexes matching the appropriate names, and regexes
-    determine the input format of function variables and 'set_var' actions. These make the input formats of the functions more flexible, and
-    rnames serve as the identifiers of variables/functions later on. Some common input-formatting-regexes are provided in Patterns, and
-    their respective converter functions are available as static functions.
-    'help' and 'exit' functions are already defined, and work properly.
-    Class variables:
-    Patterns: "enum" of str
-        Some common regex strings that match usual input: booleans and text for example.
-    _arg_pattern: compiled regex
-        Matches and extracts a string argument, with a comma possibly at the end.
-    _kwarg_pattern: compiled regex
-        Matches a single keyword argument, and extracts the name and input from it. For internal use.
-    _kwargs_pattern: compiled regex
-        Matches the **kwargs part of a function call. For internal use.'''
+    determine the input format of function variables and `'set_var'` actions. These make the input formats of the functions more flexible, and
+    `rname`s serve as the identifiers of variables/functions later on. Some common input-formatting-regexes are provided in Patterns, and
+    their respective converter functions are available as static functions.\\
+    `'help'` and `'exit'` functions are already defined, and work properly.\n
+    Class variables:\n
+    `Patterns`: "enum" of `str`
+    >   Some common regex strings that match usual input: booleans and text for example.\n
+    `_arg_pattern`: compiled regex
+    >   Matches and extracts a string argument, with a comma possibly at the end.\n
+    `_kwarg_pattern`: compiled regex
+    >   Matches a single keyword argument, and extracts the name and input from it. For internal use.\n
+    `_kwargs_pattern`: compiled regex
+    >   Matches the **kwargs part of a function call. For internal use.'''
     class Patterns:
         '''Contains constant texts which match common patterns.
-        BOOL:      match a boolean variable written as 'true' or 'false' with different capitalizations.
-        ONOFF:     match a boolean variable written as 'on' or 'off' with different capitalizations.
-        BOOLONOFF: match a boolean variable written in any of the two ways above
-        TEXT:      match a text which may be quoted (necessary if it contains whitespace)'''
+        -   `BOOL`:      match a boolean variable written as `'true'` or `'false'` with different capitalizations.
+        -   `ONOFF`:     match a boolean variable written as `'on'` or `'off'` with different capitalizations.
+        -   `BOOLONOFF`: match a boolean variable written in any of the two ways above
+        -   `TEXT`:      match a text which may be quoted (necessary if it contains whitespace)
+        -   `QUOTETEXT`: match a text which is quoted by `'` or `"`'''
         BOOL = r'(?:true|True|TRUE|false|False|FALSE)'
         ONOFF = r'(?:on|On|ON|off|Off|OFF)'
         BOOLONOFF = r'''[^'"\s]\S*|'.+?(?<!\\)(?:\\\\)*'|".+?(?<!\\)(?:\\\\)*"'''
         TEXT = r'''[^'"\s]\S*|'.+?(?<!\\)(?:\\\\)*'|".+?(?<!\\)(?:\\\\)*"'''
+        QUOTETEXT = r''''.+?(?<!\\)(?:\\\\)*'|".+?(?<!\\)(?:\\\\)*"'''
     _arg_pattern = re.compile(r'''([^'"\s]\S*|'.+?(?<!\\)(?:\\\\)*'|".+?(?<!\\)(?:\\\\)*")[,;]{0,1}''')
     _kwarg_pattern = re.compile(r'''\s+(?P<name>\w+)=(?P<v>[^'"\s]\S*|'.+?(?<!\\)(?:\\\\)*'|".+?(?<!\\)(?:\\\\)*")[,;]{0,1}''')
     _kwargs_pattern = re.compile(r'''(?:\s+\w+=(?:[^'"\s]\S*|'.+?(?<!\\)(?:\\\\)*'|".+?(?<!\\)(?:\\\\)*")[,;]{0,1})*\s*$''')
 
     def __init__(self, cursor='> ', reply_to_nonsense='', help_cmd='help', exit_cmd=r'quit|exit', 
         description=None, welcome_text="<Type 'help' for help.>"):
-        '''cursor: str
-            String displayed in the input prompt.
-        reply_to_nonsense: str
-            What will be printed, when the input is not a recognised command. No newline will be added at the end.
-        help_cmd: str (regex)
-            rname of the help command. If None, the built-in help will be disabled.
-        exit_cmd: str (regex)
-            rname of the exit command. If None, the built-in exit function will be disabled.
-        description: str or None
-            A short description of what this interactive app does. This will be printed at the beginning of 'help' calls.
-        welcome_text: str
-            This will be printed before the first input request.'''
+        '''`cursor`: `str`
+        >   String displayed in the input prompt.\n
+        `reply_to_nonsense`: `str`
+        >   What will be printed, when the input is not a recognised command. No newline will be added at the end.\n
+        `help_cmd`: `str` (regex)
+        >   `rname` of the help command. If `None`, the built-in help will be disabled.\n
+        `exit_cmd`: `str` (regex)
+        >   `rname` of the exit command. If `None`, the built-in exit function will be disabled.\n
+        `description`: `str`
+        >   A short description of what this interactive app does. This will be printed at the beginning of `'help'` calls.\n
+        `welcome_text`: `str`
+        >   This will be printed before the first input request.'''
         self.vars = []
         self.funcs = []
         self.cursor = cursor
@@ -74,11 +77,11 @@ class ConsoleApp:
             self.add_function(exit_cmd,[],description='Exit the program.')
     
     def add_variable(self, rname, variable_pattern, description=''):
-        '''Add a variable that can be parsed by the interpreter.
-        rname: str
-            Regex pattern that matches the name of this variable
-        variable_pattern: str
-            Regex pattern that describes how the input should be formatted. Contains a single group, which will be returned when we
+        '''Add a variable that can be parsed by the interpreter.\n
+        `rname`: `str`
+        >   Regex pattern that matches the name of this variable\n
+        `variable_pattern`: `str`
+        >   Regex pattern that describes how the input should be formatted. Contains a single group, which will be returned when we
             set the variable.'''
         self.vars.append({'rname': rname,
             'variable_pattern': variable_pattern,
@@ -88,19 +91,19 @@ class ConsoleApp:
             'set_pattern': re.compile(rname+r'(?:\s*=\s*|\s+?)('+variable_pattern+')\s*')})
     
     def add_function(self, rname, signature, *flags, has_args=False, has_kwargs=False, description=''):
-        '''Add a function that can be parsed by the interpreter.
-        rname: str
-            Regex pattern that matches this command (e.g. r'k[-_]opt(imi[sz]ation)?')
-        signature: list[tuple(rname, variable_pattern[, default_value])]
-            Function signature. Each element of the list represents a parameter with a tuple, whose first entry is a Regex-name
+        '''Add a function that can be parsed by the interpreter.\n
+        `rname`: `str`
+        >   Regex pattern that matches this command (e.g. `r'k[-_]opt(imi[sz]ation)?'`)\n
+        `signature`: `list[tuple(rname, variable_pattern[, default_value])]`
+        >   Function signature. Each element of the list represents a parameter with a tuple, whose first entry is a Regex-name
             for the variable, the second is a pattern for the argument (the entire matched pattern will be returned), and the third 
-            (optional) one the default value.
-        has_args: bool
-            Does this function have an *args argument?
-        has_kwargs: bool
-            Does this function have a **kwargs argument?
-        *flags: strings
-            Possible flags for the function (Regex-names).'''
+            (optional) one the default value.\n
+        `has_args`: `bool`
+        >   Does this function have an `*args` argument?\n
+        `has_kwargs`: `bool`
+        >   Does this function have a `**kwargs` argument?\n
+        `*flags`: strings
+        >   Possible flags for the function (Regex-names).'''
         self.funcs.append({'rname': rname, 
             'signature': signature, 
             'has_args': has_args, 
@@ -123,26 +126,26 @@ class ConsoleApp:
         return self.parse(input(self.cursor))
 
     def parse(self, string):
-        '''Parse a string and decode it into a variable assignment or a function call.
+        '''Parse a string and decode it into a variable assignment or a function call.\n
         If it is a variable name without parameters, it will return 
-            'get_var', rname, None
+        >   `'get_var', rname, None`\n
         If it is a variable name with a properly formatted parameter, it will return
-            'set_var', rname, contents_of_the_first_group
+        >   `'set_var', rname, contents_of_the_first_group`\n
         If it is a variable name with an improperly formatted parameter, it will print an error, and return
-            'fail_var', raname, input_string
+        >   `'fail_var', raname, input_string`\n
         If it is a function name, than the first few arguments will be matched against the function's flags. The remaining parameters
         will be parsed in a python-function-pattern-style, with spaces between arguments. This means that the next few parameters will
-        be considered positional input and matched against the respective input-patterns. Additional parameters will go into *args.
-        name=value assignments can also set parameters, and unknown names will be passed to **kwargs.
+        be considered positional input and matched against the respective input-patterns. Additional parameters will go into `*args`.
+        `name=value` assignments can also set parameters, and unknown names will be passed to `**kwargs`.\\
         If the input is properly formatted, it will return
-            'func', rname, {'flags': {flag_rname_0: bool, flag_rname_1: bool, ...},
-                'params': {param_rname_0: str, param_rname_1: str, ...}[,
+        >   `'func', rname, {'flags': {flag_rname_0: bool, flag_rname_1: bool, ...},
+                'params': {param_rname_0: str, param_rname_1: str, ...}[,\\
                 '*args': [additional_arg_0, additional_arg_1, ...],
-                '**kwargs': {name_0: str, name_1: str, ...}]}
+                '**kwargs': {name_0: str, name_1: str, ...}]}`\n
         If the input is improperly formatted, it will print an error, and return:
-            'fail_func', rname, input_string
+        >   `'fail_func', rname, input_string`\n
         If the input doesn't match any of the variables/functions, it will return:
-            'nonsense', None, None'''
+        >   `'nonsense', None, None`'''
         # Match variables
         for v in self.vars:
             if v["name_pattern"].match(string) is not None:
@@ -221,7 +224,7 @@ class ConsoleApp:
             (' **kwargs' if f['has_kwargs'] else '')
     
     def help_func(self, *args):
-        '''Display help for the app if args is empty, otherwise for the given variables and functions.'''
+        '''Display help for the app if `*args` is empty, otherwise for the given variables and functions.'''
         if len(args) == 0:
             if self.description is not None and self.description != '':
                 print(self.description)
@@ -246,7 +249,7 @@ class ConsoleApp:
     # UTIL
     @staticmethod
     def get_text(s):
-        '''Returns the text contained in s, with the quotes and escapes removed.'''
+        '''Returns the text contained in `s`, with the quotes and escapes removed.'''
         if len(s) == 0:
             return s
         if s[0] in '\'"':
@@ -265,8 +268,8 @@ class ConsoleApp:
     
 
 def solve_regex(pattern_string):
-    '''Return a "typical" string which satisfies the given (not-too-complicated) regex.
-    Can deal with (?:), [], \s\d\w\S\D\W, ., ?, +, *, |'''
+    '''Return a "typical" string which satisfies the given (not-too-complicated) regex.\\
+    Can deal with `(?:)`, `[]`, `\s\d\w\S\D\W`, `.`, `?`, `+`, `*`, `|`'''
     def process_brackets(match):
         '''Processes the contents of a match for a [...] instance. Supposes that \s\w\d\S\D\W have been replaced, but *+?\ have not.'''
         if match.group('c')[0] == r'\\':
